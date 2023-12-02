5d6dbe04ba10aa32c8a767f20f16195bf6f63345
AMIRREZAYV <amirrezayousefvand82@gmail.com>
2023-12-02 21:30:37 +0330

    AP_1402_2_5

---
+++ AP_1402_2_5/AP_1402_2_5_a/AP_1402_2_5_a.cs
@@ -0,0 +1,71 @@
+﻿using project1;
+using System;
+namespace prj
+{
+    class Test
+    {
+        public static void Main()
+        {
+            Teacher th = new Teacher("123", "Ali", "Ahmadi", 40, 40000);
+            double pay = th.Paywent();
+            Console.WriteLine(pay);
+        }
+    }
+}
+namespace project1
+{
+    public class Teacher
+    {
+        private string id;
+        private string name;
+        private string lastname;
+        private int hour;
+        private int payperonehour;
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public int Hour
+        {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Payperonehour
+        {
+            get { return payperonehour; }
+            set
+            {
+                if (value > 0)
+                { payperonehour = value; }
+            }
+        }
+
+        public Teacher() { }
+        public Teacher(string ID, string Name, string Lastname, int Hour, int Payperonehour)
+        {
+            id = ID;
+            name = Name;
+            lastname = Lastname;
+            hour = Hour;
+            payperonehour = Payperonehour;
+        }
+    
+        public double Paywent()
+        {
+            return hour * payperonehour;
+        }
+    }
+    
+ }
---
+++ AP_1402_2_5/AP_1402_2_5_b/AP_1402_2_5_b.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace prj
+{
+    class Circle
+    {
+        double pi = 3.14;
+        private double r;
+
+        public double R
+        {
+            get { return r; }
+            set { if (value > 0)
+                {
+                    r = value;
+                } 
+            }
+        }
+        public Circle() { }
+        public Circle(double R)
+        {
+            r=R;
+        }
+        public double S()
+        {
+            double Res = pi * Math.Pow(r,2);
+            return Res;
+        }
+        public double A()
+        {
+            double Res = 2 * pi * r;
+            return Res;
+        }
+    }
+    class Test
+    {
+        public static void Main()
+        {
+            Circle C = new Circle(12);
+            double a = C.A();
+            double s = C.S();
+
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+        }
+    }
+}
